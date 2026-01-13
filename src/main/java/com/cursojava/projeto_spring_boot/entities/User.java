78744e5f2f254e15bf192943f3c90fbe98036615
ApenasGui <guilherme.s.costa1@gmail.com>
2026-01-13 17:51:02 -0300

    H2 database, test profile and JPA

--- src/main/java/com/cursojava/projeto_spring_boot/entities/User.java
+++ src/main/java/com/cursojava/projeto_spring_boot/entities/User.java
@@ -1,10 +1,16 @@
 package com.cursojava.projeto_spring_boot.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
